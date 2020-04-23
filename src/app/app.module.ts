e01bcf09b228911286e27b833fce6c76cb7997cb
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-23 17:45:31 -0300

    Componente de Edição criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 
 import { TasksComponent } from './tasks/tasks.component';
 import { MaterialModule } from './material/material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EditTaskComponent } from './edit-task/edit-task.component'
 
 
 @NgModule({
   declarations: [
     AppComponent,
     TasksComponent,
+    EditTaskComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/edit-task/edit-task.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Todo } from '../todo'
+import { TodoDataService } from '../todo-data.service';
+
+@Component({
+  selector: 'app-edit-task',
+  templateUrl: './edit-task.component.html',
+  styleUrls: ['./edit-task.component.css'],
+  providers: [TodoDataService]
+})
+export class EditTaskComponent implements OnInit {
+
+  constructor(private todoDataService: TodoDataService) { }
+
+  ngOnInit(): void {
+    
+  }
+
+  get todos(){
+    return this.todoDataService.getAllTodos();
+  }
+  
+
+}
