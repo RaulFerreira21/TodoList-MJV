bc837dd55bae09e715c0c81eb1e22a2c7f6c1a75
Raul Ferreira <raul.ferreira.2@hotmail.com>
2020-04-23 18:38:24 -0300

    adicionando caminho para rota

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EditTaskComponent } from './edit-task/edit-task.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'app-edit-task', component: EditTaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
